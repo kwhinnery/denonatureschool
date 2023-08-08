a2c0d6887641154c9430b8fd726b5e78ac9f7739
Kevin Whinnery <kevin.whinnery@gmail.com>
2023-08-08 06:33:10 -0700

    remove npm deps for now

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -1,5 +1,3 @@
-// @deno-types="npm:@types/react@17"
-import React from "react";
 import { useSignal } from "@preact/signals";
 
 const IMAGES = [
@@ -8,7 +6,7 @@ const IMAGES = [
   "/dino3.jpeg",
 ];
 
-function CarouselButton(props: React.ComponentPropsWithoutRef<"button">) {
+function CarouselButton(props: any) {
   return (
     <button class="p-4 m-1" {...props}>
       {props.children}
